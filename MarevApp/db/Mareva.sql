7aea78059c0c8237913769c4cfc7183005694d00
Luka Fortun <ayn0s@pm.me>
2025-02-27 12:59:12 +0100

    Added SQL schemes of the app backend

---
+++ MarevApp/db/Mareva.sql
@@ -0,0 +1,29 @@
+CREATE TABLE `Pecheur` (
+  `PecheurId` int PRIMARY KEY AUTO_INCREMENT,
+  `Nom` TEXT
+);
+
+CREATE TABLE `Sargasse` (
+  `SargasseId` int PRIMARY KEY AUTO_INCREMENT,
+  `DetectionId` int NOT NULL,
+  `PecheurId` int NOT NULL,
+  `TailleEstimee` int
+);
+
+CREATE TABLE `Bouee` (
+  `BoueeId` int PRIMARY KEY AUTO_INCREMENT,
+  `Longitude` float NOT NULL,
+  `Latitude` float NOT NULL
+);
+
+CREATE TABLE `Detection` (
+  `SargasseId` int NOT NULL,
+  `BoueeId` int NOT NULL,
+  `HoraireDetection` Date NOT NULL
+);
+
+ALTER TABLE `Pecheur` ADD FOREIGN KEY (`PecheurId`) REFERENCES `Sargasse` (`PecheurId`);
+
+ALTER TABLE `Detection` ADD FOREIGN KEY (`SargasseId`) REFERENCES `Sargasse` (`SargasseId`);
+
+ALTER TABLE `Detection` ADD FOREIGN KEY (`BoueeId`) REFERENCES `Bouee` (`BoueeId`);
