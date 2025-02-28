ec00779904cfd130cc2d0abde1cb446454cd9018
MarwanLeGoat <122889177+MarwanLeGoat@users.noreply.github.com>
2025-02-28 09:26:02 +0100

    ajout de leaftlet

---
+++ MarevApp/frontend/src/MapComponent.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Exemple : coordonnées de Paris
+const centerParis = [48.8566, 2.3522];
+
+// Icône personnalisée (optionnel)
+const customIcon = new L.Icon({
+  iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-red.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34]
+});
+
+function MapComponent() {
+  return (
+    <MapContainer
+      center={centerParis}
+      zoom={12}
+      scrollWheelZoom={true}
+      style={{ height: "500px", width: "100%" }}
+    >
+      {/* Couche de tuiles OpenStreetMap */}
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+      {/* Marqueur (facultatif) */}
+      <Marker position={centerParis} icon={customIcon}>
+        <Popup>Bonjour Paris !</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default MapComponent;
